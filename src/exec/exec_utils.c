67aa727f8043c1d41030f05ff74da1809b7ee363
Arthur <arthurpnlcontact1@gmail.com>
2025-08-12 16:35:46 +0200

    Add utility functions and initialize pipeline structure

--- src/exec/exec_utils.c
+++ src/exec/exec_utils.c
@@ -97,4 +97,40 @@ void	ft_putstr_fd(char *s, int fd)
 	}
 }
 
-// faire fonction qui check si le path envoye est valide
+
+int	ft_strcmp(const char *s1, const char *s2)
+{
+	size_t	i;
+
+	i = 0;
+	while (s1[i] && s2[i] && s1[i] == s2[i])
+		i++;
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+}
+
+void	ft_putchar_fd(char c, int fd)
+{
+	write(fd, &c, 1);
+}
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*ptr;
+	size_t	total;
+
+	total = count * size;
+	ptr = malloc(total);
+	if (!ptr)
+		return (NULL);
+	memset(ptr, 0, total);
+	return (ptr);
+}
+
+void	init_pipeline(t_pipeline *pipeline)
+{
+    pipeline->cmd_count = count_commands(pipeline->cmd_list);
+    pipeline->pipes = create_pipes(pipeline->cmd_count);
+    pipeline->pids = malloc(sizeof(int) * pipeline->cmd_count);
+    pipeline->env = env;
+    pipeline->last_status = 0;
+}
