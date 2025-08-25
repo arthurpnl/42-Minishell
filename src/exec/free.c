f071e1ed44230fdeb22d494180d480ce1fce60d8
Arthur <arthurpnlcontact1@gmail.com>
2025-08-25 14:35:26 +0200

    Rename `count_words` to `count_words_ex` for improved clarity

--- src/exec/free.c
+++ src/exec/free.c
@@ -13,3 +13,21 @@ void	free_pipes(int *pipes, int count)
     }
     free(pipes);
 }
+
+void	close_all_pipes(int **pipes, int count)
+{
+    int    i;
+
+    i = 0;
+    if (!pipes)
+        return ;
+    while (i < count - 1)
+    {
+        if(pipes[i])
+        {
+            close(pipes[i][0]);
+            close(pipes[i][1]);
+        }
+        i++;
+    }
+}
